7f54a2a08d7ae1932c4ea75a58921cc99e1802df
Daniel Vargas <tefyvargas33@gmail.com>
2024-01-25 03:59:48 -0500

    Event listeners para tener un html más limpio

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 const day = document.getElementById('day');
 const month = document.getElementById('month');
 const year = document.getElementById('year');
+const calculateBtn = document.getElementById('calculate');
 const date = new Date();
 const actualDay = date.getDate();
 const actualMonth = date.getMonth() + 1; // Le sumo uno ya que el metodo getMonth() retorna un valor desde 0 hasta 11.
 const actualYear = date.getFullYear();
+
+// Adding an event listener to the button:
+
+calculateBtn.addEventListener('click', ageCalculator);
+
 
 function ageCalculator(){
     let personDay = day.value;
     let personMonth = month.value;
     let personYear = year.value;
     timeChecker(personDay, personMonth, personYear)
-    let personBirth = `${personDay}-${personMonth}-${personYear}`
-    console.log(personBirth)
+    // let personBirth = `${personDay}-${personMonth}-${personYear}`
+    
 }
 
 function timeChecker(dayTo, monthTo, yearTo){
     const dayFebruary = 29;
     if(dayTo > dayFebruary){
         console.log("No puedes poner valores del futuro")
+    }
+    if(monthTo > 12){
+        console.log("No hay más de 12 meses")
+    }
+    if(yearTo > actualYear){
+        console.log("No puedes nacer en el futuro")
     }
 }
 
